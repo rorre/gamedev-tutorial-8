5e59c3f76d75ebd3ba7197fa05fe4d697d38fa9b
Ren <renrror@gmail.com>
2024-04-04 10:50:22 +0700

    Initial commit

---
+++ scripts/Area2D.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+export(String) var sceneName = "Level 1"
+
+
+func _on_Area_Trigger_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene(str("res://scenes/" + sceneName + ".tscn"))
